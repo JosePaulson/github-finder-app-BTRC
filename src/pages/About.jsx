511409049e0f71762d3374b2e1ddde4a1fc3ed4d
Paulson Jose <paul@hackermail.com>
2022-06-12 03:47:18 +0530

    Format email address

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ function About() {
         <div>
             <h1 className="text-6xl">Github Finder</h1>
             <div className="pl-1 mt-4">
-                <p className="text-3xl mt-1 text-primary-content">hey@Kerasoft.in</p>
+                <p className="text-3xl mt-1 text-primary-content">hey@kerasoft.in</p>
                 <p className="text-2xl">Version: V1.0.3</p>
                 <p className="text-2xl">Developer: Paulson Jose</p>
             </div>
